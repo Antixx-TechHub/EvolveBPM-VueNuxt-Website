414fd5a35975e2b115288aca33a52b22735b28c0
AntixxTechHub <info@antixxtechhub.com>
2024-02-03 21:15:51 +0530

    CTA frame done

--- components/index/HomeCTA.vue
+++ components/index/HomeCTA.vue
@@ -6,12 +6,17 @@
             <div class="row align-items-center">
                 <div class="col-xxl-8 col-xl-8 col-lg-8">
                     <h3 class="cta__title-5">Get New Insights Weekly</h3>
-                    <div class="cta__content-5"><span>Newsletter dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt. Enter your email</span>
+                    <div class="cta__content-5"><span>Newsletter dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod <br>tempor incididunt. Enter your email.</span>
                     </div>
                 </div>
                 <div class="col-xxl-4 col-xl-4 col-lg-4">
-                    <div class="cta__btn-5 text-lg-end"><a href="/contact" class="tp-btn-orange-2">Get in Touch</a></div>
+                    <div class="cta__btn-5 text-lg-end">
+                        <nuxt-link href="/" class="tp-btn btn-cta bg-btn lnk">
+                            Request A Demo 
+                            <i class="fas fa-chevron-right fa-icon"></i>
+                  <span class="circle"></span>
+                        </nuxt-link></div>
                 </div>
             </div>
         </div>
