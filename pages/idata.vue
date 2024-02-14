8b8e8b925d00f2a6a07ef431b2a0d515c18a1f59
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-14 11:30:30 +0530

    idata header done

--- pages/idata.vue
+++ pages/idata.vue
@@ -1,14 +1,19 @@
 <template>
-  <LayoutOne>
+    <header-one :top_bar="false" :commonOffcanvas="true" />
+    <IdataTopBar />
     <IdataStatastics />
     <HomeCTA />
-  </LayoutOne>
+    <Footer />
+
 </template>
 
 <script setup>
-import LayoutOne from '~/layouts/LayoutOne.vue';
+import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
+import IdataTopBar from '~/components/idata/IdataTopBar.vue';
 import IdataStatastics from '~/components/idata/IdataStatastics.vue';
-import HomeCTA from "~/components/index/HomeCTA.vue";
+import HomeCTA from "~/components/index/HomeCTA.vue"; 
+import Footer from '~~/layouts/footers/Footer.vue';
+
 
 useHead({
   title: "EvolveBPM - Revenue Impact Solution in USA",
