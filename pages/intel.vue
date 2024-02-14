ee0d71dedd7023aa8ba5f61c6ce30fb3ca56329e
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-14 11:41:26 +0530

    ilntello page done

--- pages/intel.vue
+++ pages/intel.vue
@@ -1,14 +1,19 @@
 <template>
-  <LayoutOne>
+    <header-one :top_bar="false" :commonOffcanvas="true" />
+    <IntelloTopBar />
     <IntelStatastics />
     <HomeCTA />
-  </LayoutOne>
+    <Footer />
+
 </template>
 
 <script setup>
-import LayoutOne from '~/layouts/LayoutOne.vue';
+import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
+import IntelloTopBar from '~/components/inltel/IntelloTopBar.vue';
 import IntelStatastics from '~/components/inltel/IntelStatastics.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
+import Footer from '~~/layouts/footers/Footer.vue';
+
 
 useHead({
   title: "EvolveBPM - Revenue Impact Solution in USA",
