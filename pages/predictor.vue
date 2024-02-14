13ee1d02bec712e264fb5435c1484660cc4d205e
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-14 11:47:08 +0530

    predictor header done

--- pages/predictor.vue
+++ pages/predictor.vue
@@ -1,14 +1,19 @@
 <template>
-  <LayoutOne>
+    <header-one :top_bar="false" :commonOffcanvas="true" />
+    <PredictorTopBar />
     <PredictorFirstFrame />
     <HomeCTA />
-  </LayoutOne>
+    <Footer />
+
 </template>
 
 <script setup>
-import LayoutOne from '~/layouts/LayoutOne.vue';
+import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
+import PredictorTopBar from '~/components/predictor/PredictorTopBar.vue';
 import PredictorFirstFrame from '~/components/predictor/PredictorFirstFrame.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
+import Footer from '~~/layouts/footers/Footer.vue';
+
 
 useHead({
   title: "EvolveBPM - Revenue Impact Solution in USA",
