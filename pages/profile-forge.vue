d27145a9e2d377309869293ecadbdafc08fa10de
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-14 12:14:53 +0530

    profile forge done

--- pages/profile-forge.vue
+++ pages/profile-forge.vue
@@ -1,14 +1,19 @@
 <template>
-  <LayoutOne>
+    <header-one :top_bar="false" :commonOffcanvas="true" />
+    <ProfileTopBar />
     <ProfileForgeFirstFrame />
     <HomeCTA />
-  </LayoutOne>
+    <Footer />
+
 </template>
 
 <script setup>
-import LayoutOne from '~/layouts/LayoutOne.vue';
+import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
+import ProfileTopBar from '~/components/profile-forge/ProfileTopBar.vue';
 import ProfileForgeFirstFrame from '~/components/profile-forge/ProfileForgeFirstFrame.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
+import Footer from '~~/layouts/footers/Footer.vue';
+
 
 useHead({
   title: "EvolveBPM - Revenue Impact Solution in USA",
