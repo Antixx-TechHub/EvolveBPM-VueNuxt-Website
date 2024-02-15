a4e0934203cab9633f149cd5f0a2c6760a663abb
AntixxTechHub <info@antixxtechhub.com>
2024-02-15 15:11:04 +0530

    New footer social page name added

--- components/social/Social.vue
+++ components/social/Social.vue
@@ -33,7 +33,13 @@ export default {
           name: "Linkedin",
         },
         {
-          link: "https://www.youtube.com/",
+          link: "https://www.instagram.com/evolvebpm/",
+          target: "_blank",
+          icon: "fa-brands fa-instagram",
+          name: "Youtube",
+        },
+        {
+          link: "https://www.youtube.com/@evolvebpmllc7151/videos",
           target: "_blank",
           icon: "fa-brands fa-youtube",
           name: "Youtube",
