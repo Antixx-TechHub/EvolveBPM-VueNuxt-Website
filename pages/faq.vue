0d9479cc346619ba720ab7acfbe16063c53dbbb9
AntixxTechHub <info@antixxtechhub.com>
2024-02-27 09:19:11 +0530

    FAQ component added

--- pages/faq.vue
+++ pages/faq.vue
@@ -2,6 +2,7 @@
   <div>
     <LayoutOne>
     <!-- <header-one :top_bar="false" :header_solid="true" :commonOffcanvas="true" /> -->
+    <about-top-bar />
     <faq-breadcrumb/>
     <faq-thumb/>
     <faq-area/>
@@ -14,6 +15,7 @@
 <script>
 // import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import LayoutOne from '~/layouts/LayoutOne.vue';
+import AboutTopBar from "~~/components/faqs/AboutTopBar.vue";
 import FaqBreadcrumb from '~~/components/breadcrumb/FaqBreadcrumb.vue';
 import FaqThumb from '~~/components/faqs/FaqThumb.vue';
 import FaqArea from '~~/components/faqs/FaqArea.vue';
@@ -24,6 +26,7 @@ export default {
   components: {
     // HeaderOne,
     LayoutOne,
+    AboutTopBar,
     FaqBreadcrumb,
     FaqThumb,
     FaqArea,
