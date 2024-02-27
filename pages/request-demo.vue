65a8b937ab55393295c3cafd52c2ac90a6d75351
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-27 10:57:26 +0530

    request demo is set

--- pages/request-demo.vue
+++ pages/request-demo.vue
@@ -1,24 +1,31 @@
 <template>
-    <header-one :top_bar="false" :commonOffcanvas="true" />
-    <DemoTopBar />
-    <contact-area/>
-    <HomeCTA />
-    <Footer />
+  <header-one :top_bar="false" :commonOffcanvas="true" />
+  <DemoTopBar />
+
+  <div class="container">
+    <div class="contact__form-2">
+      <h3 class="contact__form-2-title">Send a message</h3>
+      <contact-form />
+    </div> 
+  </div>
+
+  <HomeCTA />
+  <Footer />
 </template>
-
-<script setup>
-
+<script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import DemoTopBar from '~/components/requestdemo/DemoTopBar.vue';
-import ContactArea from '~~/components/contact/ContactArea.vue';
+import ContactForm from '~/components/forms/ContactForm.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
 import Footer from '~~/layouts/footers/Footer.vue';
 
-
-useHead({
-  title: "EvolveBPM - Revenue Impact Solution in USA",
-});
-
-
+export default {
+  components: { HeaderOne, Footer, ContactForm, HomeCTA, DemoTopBar },
+  setup() {
+    useHead({
+      title: "Blog Grid - Creative Agency & Portfolio Vue Nuxt 3 Template",
+    });
+  },
+};
 
 </script>
