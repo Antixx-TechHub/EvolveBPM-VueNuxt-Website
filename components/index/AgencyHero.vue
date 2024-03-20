27d77ef602a0cfb75918af9457a2d2f52953555e
AntixxTechHub <info@antixxtechhub.com>
2024-03-20 22:46:34 +0530

    final banner done

--- components/index/AgencyHero.vue
+++ components/index/AgencyHero.vue
@@ -1,5 +1,6 @@
 <template>
-  <section class="slider__area box-plr-5-245 slider__height-5 d-flex align-items-center hero-bg-5 p-relative z-index-1 fix">
+  <section
+    class="slider__area box-plr-5-245 pt-150 pb-50 slider__height-5 d-flex align-items-center hero-bg-5 p-relative z-index-1 fix">
     <div class="slider__shape">
       <img class="slider__shape-5" src="~/assets/img/slider/5/shape/slider-shape-1.png" alt="shape">
       <img class="slider__shape-6" src="~/assets/img/slider/5/shape/slider-shape-2.png" alt="shape">
@@ -27,43 +28,42 @@
           </div>
         </div>
         <div class="col-xxl-6 col-xl-6 col-lg-6">
-          <div class="slider__thumb-5 text-end scene-2">
-            <div :class="`brand__slider-5 ${square ? square : ''}`">
-                        <div class="brand__slider-5">
-                            <swiper class="brand__slider-active-5" :freeMode="true" :loop="true" :autoplay="{
-                                delay: 1,
-                                pauseOnMouseEnter: true,
-                            }" :slidesPerView="1" :centeredSlides="true" :centeredSlidesBounds="true" :speed="4000"
-                                :modules="modules" :shortSwipes="false" :longSwipes="false" :allowTouchMove="false"
-                                :grabCursor="false" :breakpoints="{
-                                    '0': {
-                                        slidesPerView: 1,
-                                    },
-                                    '576': {
-                                        slidesPerView: 1,
-                                    },
-                                    '768': {
-                                        slidesPerView: 2,
-                                    },
-                                    '991': {
-                                        slidesPerView: 2,
-                                    },
-                                    '1200': {
-                                        slidesPerView: 2,
-                                    },
-                                }">
-                                <swiper-slide v-for="(brand, i) in brand_1_data" :key="i">
-                                    <div class="brand__item-5">
-                                        <img :src="brand.brand" alt="brand" />
-                                    </div>
-                                </swiper-slide>
-                            </swiper>
-                        </div>
-                    </div>
+
+          <div :class="`brand__slider-5 ${square ? square : ''}`">
+            <div class="brand__slider-5">
+              <swiper class="brand__slider-active-5" :freeMode="true" :loop="true" :autoplay="{
+            delay: 3,
+            pauseOnMouseEnter: true,
+          }" :slidesPerView="2" :centeredSlides="true" :centeredSlidesBounds="true" :speed="8000" :modules="modules"
+                :shortSwipes="false" :longSwipes="false" :allowTouchMove="false" :grabCursor="false" :breakpoints="{
+            '0': {
+              slidesPerView: 1,
+            },
+            '576': {
+              slidesPerView: 1,
+            },
+            '768': {
+              slidesPerView: 1,
+            },
+            '991': {
+              slidesPerView: 1,
+            },
+            '1200': {
+              slidesPerView: 1,
+            },
+          }">
+                <swiper-slide v-for="(brand, i) in brand_1_data" :key="i">
+                  <div class="about__gallery-thumb m-img">
+                    <img :src="brand.brand" alt="brand" />
+                  </div>
+                </swiper-slide>
+              </swiper>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
   </section>
 </template>
 
@@ -72,77 +72,63 @@
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Autoplay } from 'swiper';
 // internal
-import brand_1 from "~/assets/img/brand/5/brand-1.png";
-import brand_2 from "~/assets/img/brand/5/brand-2.png";
-import brand_3 from "~/assets/img/brand/5/brand-3.png";
-import brand_4 from "~/assets/img/brand/5/brand-4.png";
-import brand_5 from "~/assets/img/brand/5/brand-5.png";
-import brand_6 from "~/assets/img/brand/5/brand-6.png";
-import brand_7 from "~/assets/img/brand/5/brand-7.png";
-import brand_8 from "~/assets/img/brand/5/brand-8.png";
+import brand_1 from "~/assets/img/brand/1/brand_1.png";
+import brand_2 from "~/assets/img/brand/1/brand_2.png";
+import brand_3 from "~/assets/img/brand/1/brand_3.png";
+import brand_4 from "~/assets/img/brand/1/brand_4.png";
+import brand_5 from "~/assets/img/brand/1/brand_5.png";
+import brand_6 from "~/assets/img/brand/1/brand_6.png";
+import brand_7 from "~/assets/img/brand/1/brand_7.png";
+import brand_8 from "~/assets/img/brand/1/brand_8.png";
+import brand_9 from "~/assets/img/brand/1/brand_9.png";
+import brand_10 from "~/assets/img/brand/1/brand_10.png";
 
-
-
-import slider_img_1 from '~/assets/img/slider/5/slider-img-1.png';
-import slider_img_2 from '~/assets/img/slider/5/slider-img-2.png';
 import MouseMove from '../mouse-move/MouseMove.vue';
 
 export default {
   components: { MouseMove, Swiper, SwiperSlide },
-    props: {
-        spacing: {
-            type: Boolean,
-            default: false,
-        },
-        style_2: {
-            type: Boolean,
-            default: false,
-        },
-        el_style: {
-            type: Boolean,
-            default: false,
-        },
-        square: {
-            type: String,
-            default: "",
-        },
+  props: {
+    spacing: {
+      type: Boolean,
+      default: false,
     },
-    data() {
-        return {
-            brand_1_data: [
-                { brand: brand_1 },
-                { brand: brand_2 },
-                { brand: brand_3 },
-                { brand: brand_4 },
-                { brand: brand_5 },
-                { brand: brand_6 },
-                { brand: brand_7 },
-                { brand: brand_2 },
-                { brand: brand_4 },
-                { brand: brand_6 },
-                { brand: brand_1 },
-            ],
-            brand_2_data: [
-                { brand: brand_1 },
-                { brand: brand_3 },
-                { brand: brand_6 },
-                { brand: brand_5 },
-                { brand: brand_8 },
-                { brand: brand_2 },
-                { brand: brand_7 },
-                { brand: brand_2 },
-                { brand: brand_5 },
-                { brand: brand_1 },
-                { brand: brand_6 },
-            ],
-        };
+    style_2: {
+      type: Boolean,
+      default: false,
     },
-    setup() {
+    el_style: {
+      type: Boolean,
+      default: false,
+    },
+    square: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      brand_1_data: [
+        { brand: brand_1 },
+        { brand: brand_2 },
+        { brand: brand_7 },
+        { brand: brand_8 },
+        { brand: brand_3 },
+        { brand: brand_4 },
+        { brand: brand_5 },
+        { brand: brand_6 },
+        { brand: brand_7 },
+        { brand: brand_8 },
+        { brand: brand_9 },
+        { brand: brand_10 },
+      ],
+    };
+  },
+  setup() {
 
-        return {
-            modules: [Autoplay],
-        };
-    },
-    mounted() { },
+    return {
+      modules: [Autoplay],
+    };
+  },
+  mounted() { },
 }
 </script>
