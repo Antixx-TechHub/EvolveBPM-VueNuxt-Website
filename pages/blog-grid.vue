cbc555c0d0573e700010b84906d01e11e686e084
AntixxTechHub <info@antixxtechhub.com>
2024-03-21 17:22:07 +0530

    blog api

--- pages/blog-grid.vue
+++ pages/blog-grid.vue
@@ -2,7 +2,6 @@
   <div>
     <header-one :top_bar="false" :header_solid="true" :commonOffcanvas="true" />
     <BlogBreadcrumb />
-    <FeaturedBlogGridArea />
     <BlogGridArea />
     <HomeCTA />
     <Footer />
@@ -13,14 +12,13 @@
 <script>
 import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
 import BlogBreadcrumb from '~~/components/blog-grid/BlogBreadcrumb.vue';
-import FeaturedBlogGridArea from '~~/components/blog-grid/FeaturedBlogGridArea.vue';
 import BlogGridArea from '~~/components/blog-grid/BlogGridArea.vue';
 import HomeCTA from "~/components/index/HomeCTA.vue";
 import Footer from "~~/layouts/footers/Footer.vue";
 import BackToTop from '~~/layouts/footers/component/BackToTop.vue';
 
 export default {
-  components: { HeaderOne, BlogBreadcrumb, FeaturedBlogGridArea, BlogGridArea, HomeCTA, Footer, BackToTop },
+  components: { HeaderOne, BlogBreadcrumb, BlogGridArea, HomeCTA, Footer, BackToTop },
   setup() {
     useHead({
       title: "Blogs - Evolve BPM ",
