efbb2bcbd15e55b1317fd396e11f37c7755bd4d1
AntixxTechHub <info@antixxtechhub.com>
2024-03-22 14:52:19 +0530

    Our asset added

--- components/index/OurMediaAssets.vue
+++ components/index/OurMediaAssets.vue
@@ -15,17 +15,68 @@
         </div>
         <div class="col-xxl-5 col-xl-5 col-lg-5">
           <div class="skill__item-wrapper">
-            <div v-for="(skill, i) in skill_data" :key="i" class="skill__item d-flex align-items-center">
-              <div v-for="(item, i) in skill.skill_items" :key="i" class="skill__single text-center mb-20 mr-20">
-                <div class="skill__icon">
-                  <img :src="item.icon" alt="icon" />
-                </div>
-                <div class="skill__content">
-                  <h4>
-                    <span class="counter">
-                      {{ item.num }}</span>{{ item.text }}
-                  </h4>
-                </div>
+            <div :class="`brand__slider-5 ${square ? square : ''}`">
+              <div class="brand__slider-5">
+                <swiper class="brand__slider-active-5" :freeMode="true" :loop="true" :autoplay="{
+            delay: 1,
+            pauseOnMouseEnter: true,
+          }" :slidesPerView="1" :centeredSlides="true" :centeredSlidesBounds="true" :speed="4000" :modules="modules"
+                  :shortSwipes="false" :longSwipes="false" :allowTouchMove="false" :grabCursor="false" :breakpoints="{
+            '0': {
+              slidesPerView: 1,
+            },
+            '576': {
+              slidesPerView: 2,
+            },
+            '768': {
+              slidesPerView: 2,
+            },
+            '991': {
+              slidesPerView: 2,
+            },
+            '1200': {
+              slidesPerView: 2,
+            },
+          }">
+                  <swiper-slide v-for="(brand, i) in brand_1_data" :key="i">
+                    <div class="brand__item-5">
+                      <img :src="brand.brand" alt="brand" />
+                    </div>
+                  </swiper-slide>
+                </swiper>
+              </div>
+            </div>
+
+            <div :class="`brand__slider-5 pt-20 ${square ? square : 'pb-120'}`">
+              <div class="brand__slider-5">
+                <swiper ref="swiper_slide" class="brand__slider-active-5-1" :freeMode="true" :loop="true" :autoplay="{
+            delay: 1,
+            disableOnInteraction: true,
+            reverseDirection: true,
+          }" :slidesPerView="1" :centeredSlides="true" :speed="5000" :modules="modules" :shortSwipes="false"
+                  :longSwipes="false" :allowTouchMove="false" :grabCursor="false" :breakpoints="{
+            '0': {
+              slidesPerView: 1,
+            },
+            '576': {
+              slidesPerView: 2,
+            },
+            '768': {
+              slidesPerView: 2,
+            },
+            '991': {
+              slidesPerView: 2,
+            },
+            '1200': {
+              slidesPerView: 2,
+            },
+          }">
+                  <swiper-slide v-for="(brand, i) in brand_2_data" :key="i">
+                    <div class="brand__item-5">
+                      <img :src="brand.brand" alt="brand" />
+                    </div>
+                  </swiper-slide>
+                </swiper>
               </div>
             </div>
           </div>
@@ -34,8 +85,22 @@
     </div>
   </section>
 </template>
-  
+
 <script>
+// external
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay } from 'swiper';
+// internal
+import brand_1 from "~/assets/img/brand/5/brand-1.png";
+import brand_2 from "~/assets/img/brand/5/brand-2.png";
+import brand_3 from "~/assets/img/brand/5/brand-3.png";
+import brand_4 from "~/assets/img/brand/5/brand-4.png";
+import brand_5 from "~/assets/img/brand/5/brand-5.png";
+import brand_6 from "~/assets/img/brand/5/brand-6.png";
+import brand_7 from "~/assets/img/brand/5/brand-7.png";
+import brand_8 from "~/assets/img/brand/5/brand-8.png";
+
+
 //skill images
 import skill_icon_1 from "~/assets/img/skill/skill-icon-1.png";
 import skill_icon_2 from "~/assets/img/skill/skill-icon-2.png";
@@ -49,9 +114,53 @@ import skill_icon_6 from "~/assets/img/skill/skill-icon-6.png";
 import OurMediaAssetsItems from './OurMediaAssetsItems.vue';
 
 export default {
-  components: { OurMediaAssetsItems },
+  components: { OurMediaAssetsItems, Swiper, SwiperSlide },
+  props: {
+    spacing: {
+      type: Boolean,
+      default: false,
+    },
+    style_2: {
+      type: Boolean,
+      default: false,
+    },
+    el_style: {
+      type: Boolean,
+      default: false,
+    },
+    square: {
+      type: String,
+      default: "",
+    },
+  },
   data() {
     return {
+      brand_1_data: [
+        { brand: brand_1 },
+        { brand: brand_2 },
+        { brand: brand_3 },
+        { brand: brand_4 },
+        { brand: brand_5 },
+        { brand: brand_6 },
+        { brand: brand_7 },
+        { brand: brand_2 },
+        { brand: brand_4 },
+        { brand: brand_6 },
+        { brand: brand_1 },
+      ],
+      brand_2_data: [
+        { brand: brand_1 },
+        { brand: brand_3 },
+        { brand: brand_6 },
+        { brand: brand_5 },
+        { brand: brand_8 },
+        { brand: brand_2 },
+        { brand: brand_7 },
+        { brand: brand_2 },
+        { brand: brand_5 },
+        { brand: brand_1 },
+        { brand: brand_6 },
+      ],
       skill_data: [
         {
           skill_items: [
@@ -77,6 +186,12 @@ export default {
       ],
     };
   },
+  setup() {
+
+    return {
+      modules: [Autoplay],
+    };
+  },
+  mounted() { },
 };
 </script>
-  
