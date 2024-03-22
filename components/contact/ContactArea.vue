909219a19c1cb9e56dc81a61134c83702117ffa1
AntixxTechHub <info@antixxtechhub.com>
2024-03-22 10:54:23 +0530

    Final contact us page done

--- components/contact/ContactArea.vue
+++ components/contact/ContactArea.vue
@@ -1,64 +1,264 @@
 <template>
-  <contact-top-area  title="you can directly call or drop us an email!" subtitle="Connect with Us"/>
+  <contact-top-area title="you can directly call or drop us an email!" subtitle="Connect with Us" />
   <!-- contact item start -->
   <div class="contact__item-area contact__translate-2">
     <div class="container">
-        <div class="row">
-          <div class="col-xl-4 col-lg-4 col-md-6">
-              <div class="contact__item text-center mb-30 transition-3 white-bg">
-                <div class="contact__icon">
-                    <img src="~/assets/img/contact/icon/contact-icon-1.png" alt="">
-                </div>
-                <div class="contact__content">
-                    <span class="contact-item-subtitle">Contact</span>
-                    <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
-                    <p><a href="tel:+91-020-69029999">+91-020-69029999</a></p>
-                </div>
-              </div>
-          </div>
-          <div class="col-xl-4 col-lg-4 col-md-6">
-              <div class="contact__item text-center mb-30 transition-3 white-bg">
-                <div class="contact__icon">
-                    <img src="~/assets/img/contact/icon/contact-icon-3.png" alt="">
-                </div>
-                <div class="contact__content">
-                    <span class="contact-item-subtitle">Location</span>
-                    <p><a href="https://www.google.com/maps/dir//99+Wall+St+%233966,+New+York,+NY+10005,+USA/@40.7049469,-74.0076795,19.25z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c25a161aaa68c5:0x6d908c6e5aa1e52d!2m2!1d-74.0072444!2d40.7049014?entry=ttu" target="_blank">99 Wall Street #3966 New York, NY 10005.</a></p>
-                </div>
-              </div>
-          </div>
-          <div class="col-xl-4 col-lg-4 col-md-6">
-              <div class="contact__item text-center mb-30 transition-3 white-bg">
-                <div class="contact__icon">
-                    <img src="~/assets/img/contact/icon/contact-icon-2.png" alt="">
-                </div>
-                <div class="contact__content">
-                    <span class="contact-item-subtitle">Social Media</span>
-                    <p>Follow on social media</p>
-                    <div class="contact__social">
-                      <a href="https://www.facebook.com/EvolveBPM" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
-                      <a href="https://twitter.com/evolvebpm" target="_blank"><i class="fa-brands fa-twitter"></i></a>
-                      <a href="https://www.linkedin.com/company/evolve-bpm" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
-                      <a href="https://www.instagram.com/evolvebpm/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-                      <a href="https://www.youtube.com/@evolvebpmllc7151/videos" target="_blank"><i class="fa-brands fa-youtube"></i></a>
-
-                    </div>
-                </div>
-              </div>
+      <div class="row">
+        <div class="col-xl-4 col-lg-4 col-md-6">
+          <div class="contact__item text-center mb-30 transition-3 white-bg">
+            <div class="contact__icon">
+              <img src="~/assets/img/contact/icon/contact-icon-1.png" alt="">
+            </div>
+            <div class="contact__content">
+              <span class="contact-item-subtitle">Contact</span>
+              <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+              <p><a href="tel:+91-020-69029999">+91-020-69029999</a></p>
+            </div>
           </div>
         </div>
+        <div class="col-xl-4 col-lg-4 col-md-6">
+          <div class="contact__item text-center mb-30 transition-3 white-bg">
+            <div class="contact__icon">
+              <img src="~/assets/img/contact/icon/contact-icon-3.png" alt="">
+            </div>
+            <div class="contact__content">
+              <span class="contact-item-subtitle">Location</span>
+              <p><a
+                  href="https://www.google.com/maps/dir//99+Wall+St+%233966,+New+York,+NY+10005,+USA/@40.7049469,-74.0076795,19.25z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c25a161aaa68c5:0x6d908c6e5aa1e52d!2m2!1d-74.0072444!2d40.7049014?entry=ttu"
+                  target="_blank">99 Wall Street #3966 New York, NY 10005.</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-6">
+          <div class="contact__item text-center mb-30 transition-3 white-bg">
+            <div class="contact__icon">
+              <img src="~/assets/img/contact/icon/contact-icon-2.png" alt="">
+            </div>
+            <div class="contact__content">
+              <span class="contact-item-subtitle">Social Media</span>
+              <p>Follow on social media</p>
+              <div class="contact__social">
+                <a href="https://www.facebook.com/EvolveBPM" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="https://twitter.com/evolvebpm" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/company/evolve-bpm" target="_blank"><i
+                    class="fa-brands fa-linkedin-in"></i></a>
+                <a href="https://www.instagram.com/evolvebpm/" target="_blank"><i
+                    class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.youtube.com/@evolvebpmllc7151/videos" target="_blank"><i
+                    class="fa-brands fa-youtube"></i></a>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <!-- contact item end -->
 
+  <!-- contact location area start -->
+  <section class="contact__location-area pb-130 pt-110">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-5 col-lg-7 offset-xl-1 col-md-8">
+          <div class="tp-section-wrapper-2 mb-35">
+            <span class="tp-section-subtitle-2 subtitle-mb-9">LOCATIONS</span>
+            <h3 class="tp-section-title-2 font-40">Come and visit our offices around the world</h3>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-xl-10 ">
+          <div class="contact__location-item">
+            <div class="row align-items-center">
+              <div class="col-lg-9 col-md-8 col-sm-7">
+                <div class="contact__location-content d-lg-flex align-items-center">
+                  <h3 class="contact__location-title">EvolveBPM, LLC (Global Headquarters)</h3>
+
+                  <div class="contact__location-info d-sm-flex flex-wrap align-items-center">
+                    <div class="contact__location-icon mr-45">
+                      <img src="~/assets/img/contact/contact-location-1.png" alt="">
+                    </div>
+                    <div class="contact__location-content">
+                      <p>99 Wall Street #3966 New York, NY 10005.
+                      </p>
+                      <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+                      <p><a href="tel:+91-020-69029999"></a>+91-020-69029999</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-5">
+                <div class="contact__location-btn text-sm-end">
+                  <a href="/" target="_blank" class="tp-btn-border">
+                    view location
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="contact__location-item">
+            <div class="row align-items-center">
+              <div class="col-lg-9 col-md-8 col-sm-7">
+                <div class="contact__location-content d-lg-flex align-items-center">
+                  <h3 class="contact__location-title">EvolveBPM Inc. Ltd. (UK Registered Office)</h3>
+
+                  <div class="contact__location-info d-sm-flex flex-wrap align-items-center">
+                    <div class="contact__location-icon mr-45">
+                      <img src="~/assets/img/contact/contact-location-1.png" alt="">
+                    </div>
+                    <div class="contact__location-content">
+                      <p>12 Constance Street, London, England<br>E16 2DQ, GB.
+                      </p>
+                      <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+                      <p><a href="tel:+91-020-69029999"></a>+91-020-69029999</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-5">
+                <div class="contact__location-btn text-sm-end">
+                  <a href="/" target="_blank" class="tp-btn-border">
+                    view location
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="contact__location-item">
+            <div class="row align-items-center">
+              <div class="col-lg-9 col-md-8 col-sm-7">
+                <div class="contact__location-content d-lg-flex align-items-center">
+                  <h3 class="contact__location-title">EvolveBPM Inc (PHL Registered Office)</h3>
+
+                  <div class="contact__location-info d-sm-flex flex-wrap align-items-center">
+                    <div class="contact__location-icon mr-45">
+                      <img src="~/assets/img/contact/contact-location-1.png" alt="">
+                    </div>
+                    <div class="contact__location-content">
+                      <p>9th Floor, Arthaland Century Pacific Tower, <br>4th Avenue cor 30th Street Bonifacio Global<br>
+                        City, Taguig City, Philippines 1634 City.
+                      </p>
+                      <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+                      <p><a href="tel:+91-020-69029999"></a>+91-020-69029999</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-5">
+                <div class="contact__location-btn text-sm-end">
+                  <a href="/" target="_blank" class="tp-btn-border">
+                    view location
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="contact__location-item">
+            <div class="row align-items-center">
+              <div class="col-lg-9 col-md-8 col-sm-7">
+                <div class="contact__location-content d-lg-flex align-items-center">
+                  <h3 class="contact__location-title">EvolveBPM OPC PVT Ltd (INDIA Head Office)</h3>
+
+                  <div class="contact__location-info d-sm-flex flex-wrap align-items-center">
+                    <div class="contact__location-icon mr-45">
+                      <img src="~/assets/img/contact/contact-location-1.png" alt="">
+                    </div>
+                    <div class="contact__location-content">
+                      <p>Old Madras Road, 903 Brigade Paramount,<br> Bangalore, Karnataka 560093, IN.
+                      </p>
+                      <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+                      <p><a href="tel:+91-020-69029999"></a>+91-020-69029999</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-5">
+                <div class="contact__location-btn text-sm-end">
+                  <a href="/" target="_blank" class="tp-btn-border">
+                    view location
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="contact__location-item">
+            <div class="row align-items-center">
+              <div class="col-lg-9 col-md-8 col-sm-7">
+                <div class="contact__location-content d-lg-flex align-items-center">
+                  <h3 class="contact__location-title">EvolveBPM OPC PVT Ltd (INDIA Operations Unit 1)</h3>
+
+                  <div class="contact__location-info d-sm-flex flex-wrap align-items-center">
+                    <div class="contact__location-icon mr-45">
+                      <img src="~/assets/img/contact/contact-location-1.png" alt="">
+                    </div>
+                    <div class="contact__location-content">
+                      <p>BANGALORE - Ginserv, Incubator 4, 2nd Flr <br>HAL 3rd Stage, Kodihalli, Bangalore – 560008.
+                      </p>
+                      <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+                      <p><a href="tel:+91-020-69029999"></a>+91-020-69029999</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-5">
+                <div class="contact__location-btn text-sm-end">
+                  <a href="/" target="_blank" class="tp-btn-border">
+                    view location
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="contact__location-item">
+            <div class="row align-items-center">
+              <div class="col-lg-9 col-md-8 col-sm-7">
+                <div class="contact__location-content d-lg-flex align-items-center">
+                  <h3 class="contact__location-title">EvolveBPM OPC PVT Ltd (INDIA Operations Unit 2)</h3>
+
+                  <div class="contact__location-info d-sm-flex flex-wrap align-items-center">
+                    <div class="contact__location-icon mr-45">
+                      <img src="~/assets/img/contact/contact-location-1.png" alt="">
+                    </div>
+                    <div class="contact__location-content">
+                      <p>PUNE - Vatika Business Centre & Co-working <br>Spaces, 3rd Floor, Sky Belvedere, Opp
+                        <br>Symbiosis Law College, New Airport Rd, <br>Viman Nagar, Pune 411014, Maharashtra, IN.
+                      </p>
+                      <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+                      <p><a href="tel:+91-020-69029999"></a>+91-020-69029999</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-4 col-sm-5">
+                <div class="contact__location-btn text-sm-end">
+                  <a href="/" target="_blank" class="tp-btn-border">
+                    view location
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- contact location area end -->
+
   <!-- contact form start -->
-  <section class="contact__form-area pt-90">
+  <section class="contact__form-area pb-100">
     <div class="container">
       <div class="row">
         <div class="col-xl-12">
           <div class="contact__form-2">
             <h3 class="contact__form-2-title">Send a message</h3>
-            <contact-form/>
+            <contact-form />
             <p class="ajax-response"></p>
           </div>
         </div>
@@ -67,56 +267,6 @@
   </section>
   <!-- contact form end -->
 
-  <!-- contact location area start -->
-  <section class="contact__location-area pb-130 pt-110">
-    <div class="container">
-        <div class="row">
-          <div class="col-xl-5 col-lg-7 offset-xl-1 col-md-8">
-              <div class="tp-section-wrapper-2 mb-35">
-                <span class="tp-section-subtitle-2 subtitle-mb-9">LOCATIONS</span>
-                <h3 class="tp-section-title-2 font-40">Come and visit our offices around the world</h3>
-              </div>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-xl-10 ">
-              <div class="contact__location-wrapper">
-                <contact-location-item
-                title="UK Registered Office"
-                :icon="location_icon_1"
-                location="corporate@evolvebpm.com"
-                tel="+91-020-69029999"
-              />
-              <contact-location-item
-                title="PHL Registered Office"
-                :icon="location_icon_2"
-                location="corporate@evolvebpm.com"
-                tel="+91-020-69029999"
-              />
-              <contact-location-item
-                title="INDIA Head Office"
-                :icon="location_icon_3"
-                location="corporate@evolvebpm.com"
-                tel="+91-020-69029999"
-              />
-              <contact-location-item
-                title="INDIA Operations Unit 1"
-                :icon="location_icon_1"
-                location="corporate@evolvebpm.com"
-                tel="+91-020-69029999"
-              />
-              <contact-location-item
-                title="INDIA Operations Unit 2"
-                :icon="location_icon_2"
-                location="corporate@evolvebpm.com"
-                tel="+91-020-69029999"
-              />
-              </div>
-          </div>
-        </div>
-    </div>
-  </section>
-  <!-- contact location area end -->
 </template>
 
 <script>
@@ -128,10 +278,10 @@ import location_icon_2 from "~/assets/img/contact/contact-location-2.png";
 import location_icon_3 from "~/assets/img/contact/contact-location-3.png";
 import ContactLocationItem from './ContactLocationItem.vue';
 
-  export default {
+export default {
   components: { ContactTopArea, ContactForm, ContactLocationItem },
-    data () {
-      return {location_icon_1,location_icon_2,location_icon_3}
-    }
+  data() {
+    return { location_icon_1, location_icon_2, location_icon_3 }
   }
+}
 </script>
--- components/contact/ContactLocationItem.vue
+++ components/contact/ContactLocationItem.vue
@@ -3,30 +3,24 @@
     <div class="row align-items-center">
       <div class="col-lg-9 col-md-8 col-sm-7">
         <div class="contact__location-content d-lg-flex align-items-center">
-          <h3 class="contact__location-title">{{title}}</h3>
+          <h3 class="contact__location-title">EvolveBPM, LLC (Global Headquarters)</h3>
 
-          <div
-            class="contact__location-info d-sm-flex flex-wrap align-items-center"
-          >
+          <div class="contact__location-info d-sm-flex flex-wrap align-items-center">
             <div class="contact__location-icon mr-45">
-              <img :src="icon" alt="icon" />
+              <img src="~/assets/img/contact/contact-location-1.png" alt="">
             </div>
             <div class="contact__location-content">
-              <p>
-                <a href="mailto:contact.location@website.com">{{location}}</a>
-              </p>
-              <p><a href="tel:686-324-6838">{{tel}}</a></p>
+              <p>9th Floor, Arthaland Century Pacific Tower, 4th Avenue cor 30th
+                  <br>Street Bonifacio Global City, Taguig City, Philippines 1634 City.</p>
+              <p><a href="mailto:corporate@evolvebpm.com">corporate@evolvebpm.com</a></p>
+              <p><a href="tel:+91-020-69029999"></a>+91-020-69029999</p>
             </div>
           </div>
         </div>
       </div>
       <div class="col-lg-3 col-md-4 col-sm-5">
         <div class="contact__location-btn text-sm-end">
-          <a
-            href="/"
-            target="_blank"
-            class="tp-btn-border"
-            >
+          <a href="/" target="_blank" class="tp-btn-border">
             view location
           </a>
         </div>
@@ -36,12 +30,8 @@
 </template>
 
 <script>
+
 export default {
-  props:{
-    title:String,
-    icon:String,
-    location:String,
-    tel:String,
-  }
+
 };
 </script>
